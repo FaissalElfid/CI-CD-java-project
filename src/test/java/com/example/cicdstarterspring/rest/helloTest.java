512022fae5a6bbad195b5d2d739e64cd9d272c67
faissal el fid <faissal.elfid@gmail.com>
2022-01-02 19:34:18 +0100

    test

--- src/test/java/com/example/cicdstarterspring/rest/helloTest.java
+++ src/test/java/com/example/cicdstarterspring/rest/helloTest.java
@@ -1,7 +1,23 @@
 package com.example.cicdstarterspring.rest;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 class helloTest {
-
+    @Test
+    void ping_respond_OK(@Autowired MockMvc mockMvc) throws Exception {
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/hello/faissal"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("OK"));
+    }
 }
