bb4f012ad0db58c89d60710c1006c0ded1e73cfb
faissal el fid <faissal.elfid@gmail.com>
2022-01-02 19:35:46 +0100

    init

---
+++ src/main/java/com/example/cicdstarterspring/CiCdStarterSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdstarterspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdStarterSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdStarterSpringApplication.class, args);
+	}
+
+}
